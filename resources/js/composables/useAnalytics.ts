d8c8a17787dbe363cf34f8c0b24b18f0313ef6bf
Lauren Vallejo <laurxn.dev@gmail.com>
2026-02-23 11:12:06 -0500

    chore: enable local debugging for frontend-side analytics

--- resources/js/composables/useAnalytics.ts
+++ resources/js/composables/useAnalytics.ts
@@ -20,6 +20,10 @@ export function initializeAnalytics() {
     });
 
     initialized = true;
+
+    if (window.location.hostname.includes('localhost')) {
+        posthog.debug();
+    }
 }
 
 export function useAnalytics() {
